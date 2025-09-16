80ce5422d1448228e6ba26c080b79b6c263c69ae
Juan Ceballos <juandavi789@hotmail.com>
2025-09-16 18:41:42 -0500

    Initial commit

---
+++ applications/app-service/src/main/java/co/juan/plazacomidas/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.juan.plazacomidas.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.juan.plazacomidas.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
