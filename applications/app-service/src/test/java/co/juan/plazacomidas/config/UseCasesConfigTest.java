a54c9640646e2a83a7790b038342b2bb8ca4e6a9
Juan Ceballos <juandavi789@hotmail.com>
2025-09-18 22:49:21 -0500

    archivos varios de configuración y test

--- applications/app-service/src/test/java/co/juan/plazacomidas/config/UseCasesConfigTest.java
+++ applications/app-service/src/test/java/co/juan/plazacomidas/config/UseCasesConfigTest.java
@@ -1,10 +1,14 @@
 package co.juan.plazacomidas.config;
 
+import co.juan.plazacomidas.model.usuario.Usuario;
+import co.juan.plazacomidas.model.usuario.gateways.UsuarioRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UseCasesConfigTest {
@@ -33,6 +37,11 @@ public class UseCasesConfigTest {
         @Bean
         public MyUseCase myUseCase() {
             return new MyUseCase();
+        }
+
+        @Bean
+        public UsuarioRepository usuarioRepository() {
+            return Mockito.mock(UsuarioRepository.class);
         }
     }
 
