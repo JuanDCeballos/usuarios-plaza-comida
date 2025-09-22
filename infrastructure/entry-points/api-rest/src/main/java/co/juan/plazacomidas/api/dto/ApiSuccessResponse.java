43bf2b49fdd6ff4722d35268054d0d9d9354bb4a
Juan Ceballos <juandavi789@hotmail.com>
2025-09-22 15:06:34 -0500

    Se cambia el nombre del Dto de respueta para que no tenga conflictos con el dto de OpenApi

--- infrastructure/entry-points/api-rest/src/main/java/co/juan/plazacomidas/api/dto/ApiSuccessResponse.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/juan/plazacomidas/api/dto/ApiSuccessResponse.java
@@ -3,12 +3,12 @@ package co.juan.plazacomidas.api.dto;
 import lombok.Data;
 
 @Data
-public class ApiResponse<T> {
+public class ApiSuccessResponse<T> {
     private boolean success;
     private String errorMessage;
     private T data;
 
-    public ApiResponse(T data) {
+    public ApiSuccessResponse(T data) {
         this.success = true;
         this.errorMessage = null;
         this.data = data;
