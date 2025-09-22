b6177612694407ea0032ce07b07b0dbf03debbee
Juan Ceballos <juandavi789@hotmail.com>
2025-09-22 15:11:43 -0500

    Se ajustan repositorios JPA para query custom y encriptar contraseña

--- infrastructure/driven-adapters/jpa-repository/src/main/java/co/juan/plazacomidas/jpa/JpaUsuarioRepository.java
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/juan/plazacomidas/jpa/JpaUsuarioRepository.java
@@ -4,5 +4,9 @@ import co.juan.plazacomidas.jpa.entities.UsuarioEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 
+import java.util.Optional;
+
 public interface JpaUsuarioRepository extends CrudRepository<UsuarioEntity, Long>, QueryByExampleExecutor<UsuarioEntity> {
+
+    Optional<UsuarioEntity> findByCorreo(String correo);
 }
--- infrastructure/driven-adapters/jpa-repository/src/main/java/co/juan/plazacomidas/jpa/JpaUsuarioRepositoryAdapter.java
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/juan/plazacomidas/jpa/JpaUsuarioRepositoryAdapter.java
@@ -8,6 +8,7 @@ import co.juan.plazacomidas.model.usuario.Usuario;
 import co.juan.plazacomidas.model.exceptions.ResourceNotFoundException;
 import co.juan.plazacomidas.model.usuario.gateways.UsuarioRepository;
 import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -18,12 +19,15 @@ public class JpaUsuarioRepositoryAdapter extends AdapterOperations<Usuario, Usua
 
     private final JpaRolRepository jpaRolRepository;
     private final UsuarioEntityMapper usuarioEntityMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public JpaUsuarioRepositoryAdapter(JpaUsuarioRepository repository, ObjectMapper mapper,
-                                       JpaRolRepository jpaRolRepository, UsuarioEntityMapper usuarioEntityMapper) {
+                                       JpaRolRepository jpaRolRepository, UsuarioEntityMapper usuarioEntityMapper,
+                                       PasswordEncoder passwordEncoder) {
         super(repository, mapper, d -> mapper.map(d, Usuario.class));
         this.jpaRolRepository = jpaRolRepository;
         this.usuarioEntityMapper = usuarioEntityMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -34,6 +38,7 @@ public class JpaUsuarioRepositoryAdapter extends AdapterOperations<Usuario, Usua
                 .orElseThrow(() -> new ResourceNotFoundException("Rol no encontrado en la base de datos con id: " + usuario.getIdRol()));
 
         usuarioEntity.setRol(rolEntity);
+        usuarioEntity.setClave(passwordEncoder.encode(usuario.getClave()));
 
         UsuarioEntity usuarioGuardado = repository.save(usuarioEntity);
 
