2f85b2d187d9ffbea5bcf4a61a1aced57d20de2c
Juan Ceballos <juandavi789@hotmail.com>
2025-09-22 15:06:06 -0500

    Se ajustan endpoints de la aplicación

--- infrastructure/entry-points/api-rest/src/main/java/co/juan/plazacomidas/api/ApiRest.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/juan/plazacomidas/api/ApiRest.java
@@ -1,16 +1,23 @@
 package co.juan.plazacomidas.api;
 
-import co.juan.plazacomidas.api.dto.ApiResponse;
+import co.juan.plazacomidas.api.dto.ApiErrorResponse;
+import co.juan.plazacomidas.api.dto.ApiSuccessResponse;
 import co.juan.plazacomidas.api.dto.UsuarioRequestDto;
 import co.juan.plazacomidas.api.dto.UsuarioResponseDto;
 import co.juan.plazacomidas.api.utils.UsuarioMapper;
 import co.juan.plazacomidas.model.usuario.Usuario;
 import co.juan.plazacomidas.usecase.usuario.UsuarioUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,27 +28,52 @@ public class ApiRest {
     private final UsuarioUseCase usuarioUseCase;
     private final UsuarioMapper usuarioMapper;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<UsuarioResponseDto>> crearUsuario(@Valid @RequestBody UsuarioRequestDto requestDto) {
+    @Operation(summary = "Crear un nuevo propietario",
+            description = "Permite a un administrador crear un nuevo propietario de un restaurante")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Propietario creado exitosamente",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiSuccessResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Petición invalida (ej. datos del DTO incorrectos)",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiErrorResponse.class)))
+    })
+    @PostMapping("/propietario")
+    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    public ResponseEntity<ApiSuccessResponse<UsuarioResponseDto>> crearPropietario(@Valid @RequestBody UsuarioRequestDto requestDto) {
         Usuario usuario = usuarioMapper.toUsuario(requestDto);
 
         Usuario usuarioGuardado = usuarioUseCase.crearUsuario(usuario);
 
         UsuarioResponseDto responseDto = usuarioMapper.toUsuarioResponseDto(usuarioGuardado);
 
-        ApiResponse<UsuarioResponseDto> apiResponse = new ApiResponse<>(responseDto);
+        ApiSuccessResponse<UsuarioResponseDto> apiSuccessResponse = new ApiSuccessResponse<>(responseDto);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiSuccessResponse);
+    }
+
+    @PostMapping("/empleado")
+    @PreAuthorize("hasAuthority('PROPIETARIO')")
+    public ResponseEntity<ApiSuccessResponse<UsuarioResponseDto>> crearEmpleado(@Valid @RequestBody UsuarioRequestDto requestDto) {
+        Usuario usuario = usuarioMapper.toUsuario(requestDto);
+
+        Usuario usuarioGuardado = usuarioUseCase.crearUsuario(usuario);
+
+        UsuarioResponseDto responseDto = usuarioMapper.toUsuarioResponseDto(usuarioGuardado);
+
+        ApiSuccessResponse<UsuarioResponseDto> apiSuccessResponse = new ApiSuccessResponse<>(responseDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiSuccessResponse);
     }
 
     @GetMapping("/usuario/{idUsuario}")
-    public ResponseEntity<ApiResponse<UsuarioResponseDto>> obtenerById(@PathVariable("idUsuario") Long idUsuario){
+    public ResponseEntity<ApiSuccessResponse<UsuarioResponseDto>> obtenerById(@PathVariable("idUsuario") Long idUsuario) {
         Usuario usuario = usuarioUseCase.obtenerById(idUsuario);
 
         UsuarioResponseDto responseDto = usuarioMapper.toUsuarioResponseDto(usuario);
 
-        ApiResponse<UsuarioResponseDto> apiResponse = new ApiResponse<>(responseDto);
+        ApiSuccessResponse<UsuarioResponseDto> apiSuccessResponse = new ApiSuccessResponse<>(responseDto);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiSuccessResponse);
     }
 }
